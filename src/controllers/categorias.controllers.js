e0f37474ac5360b1aedddd845236a82b2e5b0f1e
Luis_hdez <luisperez@wposs.com>
2025-04-11 16:52:43 -0500

    conexion a base de datos

--- src/controllers/categorias.controllers.js
+++ src/controllers/categorias.controllers.js
@@ -1,5 +1,15 @@
-const getCategorias = (req,res) => {
-    res.json({"Categorias": "Electronicos"});
+import getConnection from "../db/database.js"
+
+const getCategorias = async (req,res) => {
+    try {
+        const categorias = await getConnection();
+        const result = await categorias.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias");
+        res.json(result);
+    } catch (error) {
+        console.log("ERROR 500");
+    }
+
+
 }
 
 export const methodHTTP = {
